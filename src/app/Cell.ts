b105ef20b6a82b38ac2eb3f90394f94014cc88b0
Nguyen Huynh <nhuynh9497@gmail.com>
2025-05-17 22:18:06 -0400

    finished game

--- src/app/Cell.ts
+++ src/app/Cell.ts
@@ -1,7 +1,13 @@
 export interface Cell{
     r: number;
     c: number;
-    selected: boolean;
+    selected: boolean;  
     val: number;
     available: boolean;
+    revealed: boolean;
+}
+
+export interface CellPair{
+    cell1: Cell | null;
+    cell2: Cell | null;
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  signal,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { gameConfig } from './configuration';
-import { Cell } from './Cell';
+import { Cell, CellPair } from './Cell';
 
 @Component({
   selector: 'app-root',
@@ -13,8 +17,11 @@ export class AppComponent implements OnInit {
   title = 'angular-memorygame-revamp';
   private row: number = gameConfig.SMALL_ROW;
   private col: number = gameConfig.SMALL_COL;
-  gameBoard: Cell[][] = [];
-  clickedCell: Cell | null = null;
+  gameBoard = signal<Cell[][]>([]);
+  cellPair: CellPair = {
+    cell1: null,
+    cell2: null,
+  };
 
   ngOnInit(): void {
     this.fillGameBoard();
@@ -22,38 +29,45 @@ export class AppComponent implements OnInit {
   }
 
   fillGameBoard(): void {
+    const tempGameBoard: Cell[][] = [];
     for (let i = 0; i < this.row; i++) {
       const cellRow: Cell[] = [];
       for (let j = 0; j < this.col; j++) {
-        cellRow.push(
-          {
-            r: i,
-            c: j,
-            selected: false,
-            val: -1,
-            available: true
-          } as Cell
-        );
+        cellRow.push(this.newCell(i, j));
       }
-      this.gameBoard.push(cellRow);
+      tempGameBoard.push(cellRow);
     }
+    this.gameBoard.set(tempGameBoard);
+  }
+
+  newCell(r: number, c: number): Cell {
+    return {
+      r: r,
+      c: c,
+      selected: false,
+      val: -1,
+      available: true,
+      revealed: false,
+    };
   }
 
   setAllCellsVals() {
     const numElems: number = Math.floor(this.row * this.col) / 2;
+    const tempGameBoard = this.gameBoard();
     for (let i = 1; i <= numElems; i++) {
-      this.setCellValRandom(i);
-      this.setCellValRandom(i);
+      this.setCellValRandom(tempGameBoard, i);
+      this.setCellValRandom(tempGameBoard, i);
     }
+    this.gameBoard.set(tempGameBoard);
   }
 
-  private setCellValRandom(val: number): void {
+  private setCellValRandom(tempGameBoard: Cell[][], val: number): void {
     let validPos: boolean = false;
     let randRow: number = Math.floor(Math.random() * this.row);
     let randCol: number = Math.floor(Math.random() * this.col);
     do {
-      if (this.gameBoard[randRow][randCol].val == -1) {
-        this.gameBoard[randRow][randCol].val = val;
+      if (tempGameBoard[randRow][randCol].val == -1) {
+        tempGameBoard[randRow][randCol].val = val;
         validPos = true;
       } else {
         randRow = Math.floor(Math.random() * this.row);
@@ -63,43 +77,82 @@ export class AppComponent implements OnInit {
   }
 
   clickCell(r: number, c: number): void {
-    if (!this.clickedCell){
-      this.clickedCell = {
-        r: r,
-        c: c,
-        selected: true,
-        val: this.gameBoard[r][c].val,
-        available: true
-      } as unknown as Cell;
-      this.gameBoard[r][c].selected = true;
+    const tempGameBoard = this.gameBoard();
+    const selectedCell = tempGameBoard[r][c];
+    if (!selectedCell.available) {
+      return;
     }
-    else{
-      const chosenCell : Cell = this.gameBoard[r][c];
-      if (r!==this.clickedCell.r || c!==this.clickedCell.c){
-        if (chosenCell.val === this.clickedCell.val ){
-          this.gameBoard[this.clickedCell.r][this.clickedCell.c].selected = false;
-          this.gameBoard[this.clickedCell.r][this.clickedCell.c].available = false;
-          chosenCell.selected = false;
-          chosenCell.available = false;
-          this.clickedCell = null;
+    selectedCell.revealed = true;
+    selectedCell.selected = true;
+
+    // if we have not selected a cell, set cell1 to this cell as first selected cell
+    if (!this.cellPair.cell1) {
+      this.cellPair.cell1 = selectedCell;
+    } else {
+      // now we have ref to 1 selected cell. Compare value of this selected cell to new selected cell
+      // make sure they are different cells
+      if (
+        selectedCell.r !== this.cellPair.cell1.r ||
+        selectedCell.c != this.cellPair.cell1.c
+      ) {
+        if (
+          !this.cellPair.cell2 &&
+          selectedCell.val === this.cellPair.cell1.val
+        ) {
+          // these cells are no longer clickable
+          // they are green now
+          selectedCell.selected = false;
+          selectedCell.available = false;
+          this.cellPair.cell1.selected = false;
+          this.cellPair.cell1.available = false;
+          this.cellPair.cell1 = null;
+        }
+
+        // new cell does not match cell1
+        else {
+          // both cell1 and cell2 are selected non-matching pair of cells.
+          // hide values of both
+          // set cell1 to newly selected cell
+          if (this.cellPair.cell2) {
+            this.cellPair.cell1.revealed = false;
+            this.cellPair.cell1.selected = false;
+            this.cellPair.cell2.revealed = false;
+            this.cellPair.cell2.selected = false;
+            this.cellPair.cell1 = selectedCell;
+            this.cellPair.cell2 = null;
+          }
+
+          // we only have 1 selected cell cell1 atm. set cell2 to newly selected cell
+          else {
+            this.cellPair.cell2 = selectedCell;
+          }
         }
       }
     }
+
+    this.gameBoard.set(tempGameBoard);
   }
 
   isCellChosen(r: number, c: number): boolean {
-    if (this.gameBoard[r][c].selected){
-      console.log(this.clickedCell !== null
-      && this.clickedCell.r === r 
-      && this.clickedCell.c === c);
-    }
-    
-    return this.clickedCell !== null
-    && this.clickedCell.r === r 
-    && this.clickedCell.c === c;
+    return this.gameBoard()[r][c].selected;
   }
 
-  isCellAvailable(r: number, c: number) : boolean {
-    return this.gameBoard[r][c].available;
+  isCellAvailable(r: number, c: number): boolean {
+    return this.gameBoard()[r][c].available;
+  }
+
+  reset() {
+    this.gameBoard.set([]);
+    this.cellPair.cell1 = null;
+    this.cellPair.cell2 = null;
+    this.fillGameBoard();
+    this.setAllCellsVals();
+  }
+
+  chooseGameBoardSize(event: any): void {
+    const size: number = event.target.value as number;
+    this.row = size;
+    this.col = size;
+    this.reset();
   }
 }
